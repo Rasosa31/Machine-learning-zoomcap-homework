5adea0dec946627fa8a9eb03015a62f6897c8ccf
Ramiro Sosa <rasosa2001@gmail.com>
2025-10-27 20:49:53 +0000

    Cambios no relevantes

--- tarea_5/test_api.py
+++ tarea_5/test_api.py
@@ -5,9 +5,9 @@ url = "http://127.0.0.1:8000/predict"
 
 client = {
     
-    "lead_source": "organic_search",
-    "number_of_courses_viewed": 4,
-    "annual_income": 80304.0
+    "lead_source": "paids_ads",
+    "number_of_courses_viewed": 2,
+    "annual_income": 79276.0
 }
 
 requests.post(url, json=client).json()
